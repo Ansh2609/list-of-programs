4fc2aee42026395c923317ffcae9b69f14b875f8
Ansh2609 <73031691+Ansh2609@users.noreply.github.com>
2023-10-11 18:45:19 -0400

    Update convert-to-letter.lisp

--- convert-to-letter.lisp
+++ convert-to-letter.lisp
@@ -7,9 +7,3 @@ CL-USER> (defun convert-to-letter (numeric-grade)
              (7 "D")
              (6 "E")
              (otherwise "F")))
-CONVERT-TO-LETTER
-CL-USER> (convert-to-letter 59)
-"F"
-CL-USER> (convert-to-letter 78)
-"D"
-CL-USER> 
